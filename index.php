896476fc8e0c664a2c19c72801b837929c989098
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-08-25 17:30:53 +0200

    db.php, index php and html (axios and vue cdn), style.css

--- index.php
+++ index.php
@@ -4,9 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Dischi PHP</title>
 </head>
 <body>
-    
+    <?php 
+        include './database.php';
+    ?>
+    <header>
+        <div class="logo">
+            <img src="" alt="">
+        </div>
+    </header>
+
+    <main>
+        <div class="wrapper">
+            <div class="album">
+                <div class="album-img">
+                    <img src="" alt="">
+                </div>
+            </div>
+
+            <div class="info">
+                <h3>Titolo</h3>
+                <p>Autore</p>
+                <p>Genere</p>
+                <p>Anno</p>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
